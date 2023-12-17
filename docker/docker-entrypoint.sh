832518b3db3033add7f924009ecb9edefdd9bcbe
Simon McWhinnie <simon.mcwhinnie@immediate.co.uk>
2023-12-17 18:06:50 +0000

    working crontab

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,7 +10,8 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
     composer install --prefer-dist --no-progress --no-suggest -o --no-interaction
     chmod -R 777 var
     ./bin/console doctrine:mongodb:schema:update || echo "Warning: failed to update DB indexes"
-    npm run build
 fi
+
+service cron start
 
 exec docker-php-entrypoint "$@"
