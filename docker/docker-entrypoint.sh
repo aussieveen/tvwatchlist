69e956ec0de2d214070333cab2c96db975aaaaa0
Simon McWhinnie <simon.mcwhinnie@immediate.co.uk>
2024-01-03 19:33:33 +0000

    more things being done

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,6 +9,7 @@ fi
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
     composer install --prefer-dist --no-progress --no-suggest -o --no-interaction --ignore-platform-reqs
     chmod -R 777 var
+    ./bin/console assets:install
     ./bin/console doctrine:mongodb:schema:update || echo "Warning: failed to update DB indexes"
 fi
 service nginx start
